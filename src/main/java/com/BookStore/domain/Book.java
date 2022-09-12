a847130ed0e9645bf269ea171407690aa867775c
Alfred <alfred.ahonen@gmail.com>
2022-09-12 19:40:36 +0300

    first commit

---
+++ src/main/java/com/BookStore/domain/Book.java
@@ -0,0 +1,56 @@
+package com.BookStore.domain;
+
+public class Book {
+	private String title;
+	private String author;
+	private String year;
+	private String isbn;
+	private Integer price;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	public Book(String title, String author, String year, String isbn, Integer price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
+	
+}
---
+++ src/main/java/com/BookStore/web/BookController.java
@@ -0,0 +1,11 @@
+package com.BookStore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/index", method=RequestMethod.GET)
+public class BookController {
+
+}
