44e9fb463643338da668d972f5c987f2919958d1
Alfred <alfred.ahonen@gmail.com>
2022-09-21 11:42:32 +0300

    category added

--- src/main/java/com/BookStore/BookStoreApplication.java
+++ src/main/java/com/BookStore/BookStoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.BookStore.domain.Book;
 import com.BookStore.domain.BookRepository;
+import com.BookStore.domain.Category;
+import com.BookStore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -16,13 +18,19 @@ public class BookStoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository){
+	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository){
 		return (args) -> {
+
+			crepository.save(new Category("Kauhu"));
+			crepository.save(new Category("Jännitys"));
+			crepository.save(new Category("Historia"));
+			crepository.save(new Category("Romanssi"));
+			crepository.save(new Category("Mysteeri"));
 			
-			repository.save(new Book("Veden alla", "Kat Falls", "2011", "9513155064", 9));
-			repository.save(new Book("Klik", "Tap", "2007", "23434", 7));
+			repository.save(new Book("Veden alla", "Kat Falls", "2011", "9513155064", 9, crepository.findByName("Jännitys").get(0)));
+			repository.save(new Book("Klik", "Tap", "2007", "23434", 7, crepository.findByName("Romanssi").get(0)));
+			
 		};
 	}
-
 
 }
--- src/main/java/com/BookStore/domain/Book.java
+++ src/main/java/com/BookStore/domain/Book.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -17,6 +19,16 @@ public class Book {
 	private String isbn;
 	private Integer price;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoryid")
+	private Category category;
+	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 	public Long getId() {
 		return id;
 	}
@@ -56,13 +68,14 @@ public class Book {
 	
 	public Book() {}
 	
-	public Book(String title, String author, String pubyear, String isbn, Integer price) {
+	public Book(String title, String author, String pubyear, String isbn, Integer price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.pubyear = pubyear;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 	}
 	
 	@Override
